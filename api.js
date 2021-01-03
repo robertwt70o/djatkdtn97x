2ed7c849eac0331b60e07779e05ea8d54ab95dbe
robertwt70o <ai35955220@sina.com>
2021-01-04 01:24:54 +0800

    master

--- api.js
+++ api.js
@@ -1,6 +1,12 @@
+var { customers } = require('./data_tier/customers');
 
 let add = (n, m) => {
     return n + m;
 };
 
+let addCustomer = (name, email, password) => {
+    customers.push({id: customers.length + 1, name: name, email: email, password: password});
+};
+
 exports.add = add;
+exports.addCustomer = addCustomer;
--- index.js
+++ index.js
@@ -12,7 +12,16 @@ application.get('/add/:n/:m', (request, response) => {
     let sum = api.add(n, m);
 
     response.send(`${n} + ${m} = ${sum}.`);
-
 });
+
+application.post('/register', (request, response) => {
+    let name = request.body.name;
+    let email = request.body.email;
+    let password = request.body.password;
+    api.addCustomer(name, email, password);
+    response.json({message: 'The customer added successfully'});
+});
+
+
 
 application.listen(port, () => console.log('Listening on port ' + port));
